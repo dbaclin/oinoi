3a0a99fe6e95470a778fd4ea018e0f844f69397f
dbaclin <dbaclin@gmail.com>
2013-05-04 00:00:16 +0200

    First version

---
+++ v0.00/upload_data.php
@@ -0,0 +1,20 @@
+<html>
+ 
+<head>    
+ 
+<!-- 1 -->
+<link href="../libs/dropzone/css/dropzone.css" type="text/css" rel="stylesheet" />
+ 
+<!-- 2 -->
+<script src="../libs/dropzone/dropzone.min.js"></script>
+ 
+</head>
+ 
+<body>
+ 
+<!-- 3 -->
+<form action="upload.php" class="dropzone"></form> 
+   
+</body>
+ 
+</html>
